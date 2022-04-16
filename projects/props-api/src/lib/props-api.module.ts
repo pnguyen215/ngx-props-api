a4c5d03f9f7e2e7c28bb9fcde7b5c9353309db85
Nguyen Huu Phuoc <nguyenhuuphuocit97@gmail.com>
2022-04-16 18:05:36 +0700

    :ok_hand: chore: config basic module props-api #3

--- projects/props-api/src/lib/props-api.module.ts
+++ projects/props-api/src/lib/props-api.module.ts
@@ -1,12 +1,35 @@
-import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ModuleWithProviders, NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { PropsApiComponent } from './props-api.component';
+import { PropsApiService } from './props-api.service';
 
 
 
 @NgModule({
   declarations: [PropsApiComponent],
   imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule
   ],
   exports: [PropsApiComponent]
 })
-export class PropsApiModule { }
+export class PropsApiModule {
+
+  public static forRoot(): ModuleWithProviders {
+
+    return {
+      ngModule: PropsApiService,
+      providers: [
+        // {
+        //   provide: CacheService,
+        //   useClass: CacheImplService
+        // }
+      ]
+    };
+  }
+
+}
